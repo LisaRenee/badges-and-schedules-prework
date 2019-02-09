810c2b1714aeea3991e2109ed4ce2b01676f484d
Lisa Scott <lrscot@aol.com>
2019-02-09 20:12:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
-# Write your code here.
+def badge_maker(name)
+ return "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(names)
+  badges = []
+  names.each do |name|
+    badges.push(badge_maker(name))
+  end
+return badges
+end
+def assign_rooms(names)
+  assignments = []
+  room = 1
+  names.each do |name|
+    assignments.push("Hello, #{name}! You'll be assigned to room #{room}!")
+    room += 1
+  end
+  assignments
+
+end
+
+def printer(attendees)
+  attendees.each do |name|
+    puts badge_maker(name) + assign_rooms(name)
+
+  end
+
+end
